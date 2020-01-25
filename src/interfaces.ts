c01b4af7af45c150c9db8013e9f5110876669b0e
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-01-25 22:32:54 +0400

    chore(Types): add extended interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+interface AuthSessionStorage extends Foxx.SessionStorage {
+  prune: () => void;
+  save: (session?: Foxx.Session) => void;
+  clear: (session: Foxx.Session) => void;
+}
+
+export interface AuthUser {
+  _id?: string;
+  _key?: string;
+  authData?: any;
+  username: string;
+}
+
+export interface AuthRequest extends Foxx.Request {
+  user: ArangoDB.Document<any>;
+  sessionStorage: AuthSessionStorage;
+}
